29667245b7e55b3df9e17970064ffb5f55e563f9
Sambhav Dwivedi <201863503+sambhavdwivediofficial@users.noreply.github.com>
2026-06-15 19:26:05 +0530

    LinkDrop

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+document.addEventListener("contextmenu", (e) => {
+  e.preventDefault();
+});
 
+document.addEventListener("keydown", (e) => {
+  if (
+    e.key === "F12" ||
+    (e.ctrlKey &&
+      e.shiftKey &&
+      ["I", "J", "C"].includes(e.key.toUpperCase())) ||
+    (e.ctrlKey && e.key.toUpperCase() === "U")
+  ) {
+    e.preventDefault();
+  }
+});
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
