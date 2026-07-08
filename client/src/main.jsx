eb4e5759e7b81085770e721d7d80610da7659f64
Sambhav Dwivedi <201863503+sambhavdwivediofficial@users.noreply.github.com>
2026-07-08 20:18:46 +0530

    LinkDrop

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,24 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-
-import { Capacitor } from "@capacitor/core";
-import { StatusBar, Style } from "@capacitor/status-bar";
-
-if (Capacitor.isNativePlatform()) {
-  StatusBar.setOverlaysWebView({
-    overlay: false,
-  });
-
-  StatusBar.setBackgroundColor({
-    color: "#070707",
-  });
-
-  StatusBar.setStyle({
-    style: Style.Dark,
-  });
-}
-
 document.addEventListener("contextmenu", (e) => {
   e.preventDefault();
 });
